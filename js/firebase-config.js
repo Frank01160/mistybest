1a28d90847658cd212e6638219a7ee686f08c17a
neoarforex <franciskinungi211@gmail.com>
2026-09-22 09:02:16 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -21,12 +21,12 @@
    ========================================================================== */
 
 const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_PROJECT.firebaseapp.com",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "REPLACE_WITH_SENDER_ID",
-  appId: "REPLACE_WITH_APP_ID",
+  apiKey: "AIzaSyAACs_nT6_AqwBdF9nSFVl-CcDiyKzhpZw",
+  authDomain: "mistybest.firebaseapp.com",
+  projectId: "mistybest",
+  storageBucket: "mistybest.firebasestorage.app",
+  messagingSenderId: "236020663058",
+  appId: "1:236020663058:web:9a1891d3c9fe9d0428e9cb",
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -55,8 +55,8 @@ db.enablePersistence({ synchronizeTabs: true }).catch((err) => {
    there's no in-app flow for it.
    ========================================================================== */
 const FIXED_EMAILS = {
-  seller: "seller@mistycode.local",
-  manager: "manager@mistycode.local",
+  seller: "mistyseller@gmail.com",
+  manager: "mistymanager@gmail.com",
 };
 
 /* Firestore structure this app expects (create the first two docs manually
